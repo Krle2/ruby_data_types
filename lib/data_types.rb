aeb9149b7ea3ee41d8d9616c39ff4f23aa0124b9
krlee <krstic.aleksandar97@gmail.com>
2023-02-19 14:19:50 +0100

    Data types

---
+++ lib/data_types.rb
@@ -0,0 +1,19 @@
+Data types:
+  STRINGS:
+    group of letters that represent a word or a sentence and it is inbetween "" or ''
+
+
+
+  Numbers
+    numbers
+  Hashes
+    dictionary {konj:horse} gives another word, you have a key and you need its value
+
+  Arrays
+    list of something inbetween [] ,list of strings, numbers etc.
+
+  Symbols
+    same as strings, just they begin with :
+
+  Booleans
+    always getting back true or false
