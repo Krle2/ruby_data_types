84aa1b989ac1da363591278d695b431f9a172fc3
krlee <krstic.aleksandar97@gmail.com>
2023-02-25 14:51:01 +0100

    Numberrsss

--- lib/numbers.rb
+++ lib/numbers.rb
@@ -1,2 +1,12 @@
-puts "Jedan plus jedan je jednakko: #{1+1}"
-puts 1 + 1
+# puts "Jedan plus jedan je jednako: #{1+1}"
+# puts 1 + 1
+# puts 1 - 1
+# puts 1 / 1
+# puts 1 * 1
+# p -4 + 5
+# ovo su neki brojevi 
+# p 1.12413 + 1.23
+puts "1.7 to integer = #{1.7.to_i}"
+puts "1.7 round = #{1.7.round}"
+puts "1 to float = #{1.to_f}"
+
