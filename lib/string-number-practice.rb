9680932c49fe0a8ae15ea9b97a2e92a9dbadb372
krlee <krstic.aleksandar97@gmail.com>
2023-02-25 15:07:13 +0100

    Practicing number codes

---
+++ lib/string-number-practice.rb
@@ -0,0 +1,10 @@
+# 1. Print me sum of 25 and 25.1
+puts "sum of 25 and 25.1 is #{25 + 25.1}"
+# 2. Convert float number 5.5 to integer
+puts "Float number 5.5 to integer is #{5.5.to_i}"
+# 3. Convert integer number 5 to float
+puts "Integer number 5 to float number is #{5.to_f}"
+# 4. Convert float number 2.5 to round
+puts "Float number 2.5 to round is #{2.5.round}"
+# 5. Puts -20 to apsolute value
+puts "-20 to absolute value is #{-20.abs}"
