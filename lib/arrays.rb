57ac9f9905c32f872026edb5cf998446d30e3d89
krlee <krstic.aleksandar97@gmail.com>
2023-02-28 15:26:30 +0100

    Arrays

---
+++ lib/arrays.rb
@@ -0,0 +1,31 @@
+array = [0, 1, 2, 3, 4]
+# .lenght = counts number of data in an array
+# puts array.length
+# .first = prints first object from an array
+# puts  array.first 
+# array.first = array[0]
+# .last = same, but last object
+# puts array.last
+# .take(number of objects) = print up to that number of objects
+# puts array.take(3)
+# .drop(which number to drop and to print rest)
+# puts array.drop(3)
+# .pop = deletes last object from an array(prints that last object)
+# array.pop
+# puts array
+# .push(number which we want to add at the end) = append << = objecgt we want to add at the end
+# puts array.push(25)
+# .delete(what we want to delete, exact number or word)
+# .delete_at(index number of what we want to delete)
+# array.delete(2)
+# puts array
+# .reverse or arr.reverse! = reverse objects in a array
+# puts array.reverse!
+# .sort or . sort! = sort in line, if we have 1,3,2 it puts  1,2,3
+
+# .select { |number| number < 4} = |variable name| (variable name comparing to the number on the right side) < (comparing to what), in code .select, we can not change actual objects, compared to codes map and each!!
+# result = array.select { |number| number < 4 }
+# puts array
+# puts result
+# p result
+
