579ef4f3ba8962966987b448062b8c411cfe0c03
krlee <krstic.aleksandar97@gmail.com>
2023-03-14 11:59:59 +0100

    New information

--- lib/arrays.rb
+++ lib/arrays.rb
@@ -1,4 +1,5 @@
 array = [0, 1, 2, 3, 4]
+arr2 = []
 # .lenght = counts number of data in an array
 # puts array.length
 # .first = prints first object from an array
@@ -28,3 +29,27 @@ array = [0, 1, 2, 3, 4]
 # puts array
 # puts result
 # p result
+
+# map and select are returning an array, while each doesnt.
+# Map and each are used to change an array, while select doesnt.
+# Iteracija(loopovanje)- iz skupa elemenata(array, string) uzimamo jedan element i nad njim vrsimo modifikovanje ili selektovanje
+
+# p array.map {|a| a+2}
+# select- filtriramo
+# p arr.select {|a| a.even?}
+# map- modifikujemo
+# p arr.each {|a| arr2 << a }
+# each- radimo nesto izvan array-a sa tim podacima
+
+# p array.include?(1)
+# include is returning boolien, so true or false
+
+# p array.join("---")
+# prima aprametre i stavlja izmedju objekata
+
+arr = [1,2,3,4,4,4,4,4,4]
+# p arr.uniq()
+# izbaci uniq elemenata
+
+# p arr << array
+# p array.concat(arr)
--- lib/data_types.rb
+++ lib/data_types.rb
@@ -19,3 +19,9 @@
 
 # -irb - interactive ruby
 # variable - are the memory locations, which holds any data you made
+
+# () koristi se za pozivanje metoda- znaci metod pa zagrada
+# [] koristi se za indeksiranje ili kreiranje array-a
+# {} koristi se za kreiranje code block-ova ili za kreiranje hasha
+
+# Blok koda - prima kod kao parametar, ne data type, poredjenja, if, else.. 
